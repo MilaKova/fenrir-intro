bc170872539093080d6ebdf3b2d6e5236489511b
Mila Kovach <mila@Milas-MacBook-Air.local>
2023-06-20 21:41:21 -0400

    add HTML form and display messages in list

--- js/index.js
+++ js/index.js
@@ -16,3 +16,36 @@ for (let i = 0; i<skills.length; i++){
     skill.innerHTML = skills[i]
     skillsList.appendChild(skill)
 }
+const messageForm = document.querySelector('[name="leave_message"]');
+const messageSection = document.getElementById('messages');
+messageSection.hidden = true;
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = e.target.usersName.value;
+    const email = e.target.usersEmail.value;
+    const message = e.target.usersMessage.value;
+  console.log("name:", name);
+  console.log("email:", email);
+  console.log("message:", message);
+
+         //----Display Messages in List
+
+  const messageList = messageSection.querySelector('ul');
+  const newMessage = document.createElement('li');
+  newMessage.innerHTML =`<a href="mailto:${email}">${name}</a>` + ' wrote: ' + `<span>${message}</span>`;
+  
+  const removeButton = document.createElement('button');
+  removeButton.innerText = 'remove';
+  removeButton.type = 'button';
+  removeButton.addEventListener('click', () => {
+     const entry = removeButton.parentNode;
+     entry.remove();
+  });
+    
+  newMessage.append(removeButton);
+  messageList.appendChild(newMessage);
+
+  messageSection.hidden = false;
+
+  messageForm.reset();
+});
